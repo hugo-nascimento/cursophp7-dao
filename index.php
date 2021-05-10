94f527f22f1e529a01a43d5b60aae4785b63d35a
hugo-nascimento <hugo-nascimento@live.com>
2021-05-10 17:00:17 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -2,12 +2,26 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega 1 usuário
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
 
-$root->loadById(1);
 
-echo $root;
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("Jo");
+//echo json_encode($search);
+
+
+//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root","!@#$%");
+
+echo $usuario;
 
 
 /*
